0721301b02c13d831428756ca8503b59bcabd246
b3-m0 <bemiyamoto24@gmail.com>
2024-10-28 20:43:31 -0700

    1/3 assets folder upload

---
+++ Scripts/changeScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public void GoToSceneTwo() {
+        SceneManager.LoadScene("startScene");
+    }
+
+    public void GoToSceneThree() {
+        SceneManager.LoadScene("lunchBoxScene");
+    }
+
+}
